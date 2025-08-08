d5c7d6dee8b587eb667ae2e7fe506f3fefeb91e8
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2025-08-09 00:57:09 +0530

    first commit

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MerkleAirdrop {
+    // Purpose:
+    // 1. Manage a list of addresses and corresponding token amounts eligible for the airdrop.
+    // 2. Provide a mechanism for eligible users to claim their allocated tokens.
+}
