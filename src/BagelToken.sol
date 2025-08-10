52bbbbe4815f2858c3995d3641ea943bf32b3068
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2025-08-10 12:17:54 +0530

    Base Airdrop contracts implemented

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -5,7 +5,9 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BagelToken is ERC20, Ownable {
-    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {
+        // The initial sypply will be managed by the owner minting tokens as needed, rather than minting a fixed supply at deployment
+    }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,8 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
-    // Purpose:
-    // 1. Manage a list of addresses and corresponding token amounts eligible for the airdrop.
-    // 2. Provide a mechanism for eligible users to claim their allocated tokens.
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    event Claim(address indexed account, uint256 amount);
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    mapping(address claimant => bool) private s_hasClaimed;
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    //CEI Followed
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // CHECK if already claimed
+        if(s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // 1. Calculate the leaf node hash
+        // This implementation double-hashes the abi.encoded data. 
+        // Consistency between off-chain leaf generation and on-chain verification is paramount. 
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+
+        // 2. Verify the Merkle Proof
+        if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+
+        s_hasClaimed[account] = true;
+        
+        // 3. Emit event
+        emit Claim(account, amount);
+
+        // 4. Transfer tokens
+        i_airdropToken.safeTransfer(account, amount);
+    }
 }
